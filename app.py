862c104ac1fad03993ed24a32932378bb475705c
M. Eduarda Souza <souza.eduarda2@escolar.ifrn.edu.br>
2024-01-10 11:11:21 -0300

    .
    .

--- app.py
+++ app.py
@@ -9,8 +9,9 @@ from flask_mysqldb import MySQL
 app = Flask(__name__)
 
 # Configurações do banco de dados
-app.config['MYSQL_USER'] = 'maluly'
-app.config['MYSQL_PASSWORD'] = 'password'
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'labinfo'
 app.config['MYSQL_DB'] = 'maluly'
 
 # Inicialização do MySQL    
@@ -70,7 +71,7 @@ def enviar():
     cursor = mysql.connection.cursor()
     cursor.execute("SELECT * FROM Cliente WHERE Email = %s AND Senha = %s", (Email, Senha))
     user = cursor.fetchone()
-    cursor.close()
+    #cursor.close()
 
     if user:
         session['Cliente'] = user[0]
@@ -110,3 +111,4 @@ def baseglow():
 if __name__ == '__main__':
     app.secret_key = 'maluly'
     app.run(debug=True)
+    
